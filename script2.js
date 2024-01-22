7755370736c53ad10c9a7a67462fc7fae63256bb
Farhad Maten <farhad.maten@gmail.com>
2024-01-22 14:07:49 -0500

    added test scripts

--- script2.js
+++ script2.js
@@ -1,29 +1,30 @@
-const getPeoplePromise = () => {
-    return fetch('https://swapi.py4e.com/api/people/')
+const getPeoplePromise = (fetchFunc) => {
+    return fetchFunc('https://swapi.py4e.com/api/people')
         .then(response => {
             return response.json();
         })
         .then(data => {
             const result = {
                 count: data.count,
-                results: data.results[0]
+                results: data.results
             }
             return result;
         })
         .catch(err => console.log(err));
 }
 
-const getPeople = async () => {
+const getPeople = async (fetchFunc) => {
     try {
-        const response = await fetch('https://swapi.py4e.com/api/people/');
+        const response = await fetchFunc('https://swapi.py4e.com/api/people');
         const data = await response.json();
         const result = {
             count: data.count,
-            results: data.results[0]
+            results: data.results
         }
         return result;
     } catch (err) {
         console.log(err);
+        return err;
     }
 }
 
--- script2.test.js
+++ script2.test.js
@@ -1,8 +1,36 @@
 const swapi = require('./script2');
 
 it('calls swapi to get people', () => {
-    swapi.getPeople().then(data => {
-        expect(data.count).toEqual(87);
+    expect.assertions(1);
+    return swapi.getPeople(fetch)
+        .then(data => {
+            expect(data.count).toEqual(87);
+        })
+});
 
-    })
-})
+it('calls swapi to get people with a promise', () => {
+    expect.assertions(2);
+    return swapi.getPeoplePromise(fetch)
+        .then(data => {
+            expect(data.count).toEqual(87);
+            expect(data.results.length).toBeGreaterThan(3);
+        });
+});
+
+it('works with mockFetch properly', () => {
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 87,
+                results: [1,2,3,4]
+            })
+        }));
+    expect.assertions(4);
+    return swapi.getPeoplePromise(mockFetch)
+        .then(data => {
+            expect(mockFetch.mock.calls.length).toBe(1);
+            expect(mockFetch).toBeCalledWith('https://swapi.py4e.com/api/people');
+            expect(data.count).toEqual(87);
+            expect(data.results.length).toBeGreaterThan(3);
+        });
+});
